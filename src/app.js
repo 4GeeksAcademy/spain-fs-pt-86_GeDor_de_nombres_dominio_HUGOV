675ecd7f11b73ec59b02926c05ad9249f6dbb748
Hvaxquez <hugoferney54@gmail.com>
2024-11-21 08:28:57 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
 };
+const pronombres = ["mi", "tu", "su"];
+const adjetivos = ["gran", "pequeño", "rápido"];
+const sustantivos = ["gato", "perro", "ratón"];
+
+function generarNombresDeDominio(pronombres, adjetivos, sustantivos) {
+  const nombresDeDominio = [];
+
+  for (let pronombre of pronombres) {
+    for (let adjetivo of adjetivos) {
+      for (let sustantivo of sustantivos) {
+        nombresDeDominio.push(`${pronombre}${adjetivo}${sustantivo}.com`);
+        log;
+      }
+    }
+  }
+
+  return nombresDeDominio;
+}
+
+const dominios = generarNombresDeDominio(pronombres, adjetivos, sustantivos);
+console.log(dominios);
